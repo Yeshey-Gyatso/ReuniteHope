e6d63764cf49a52e8fba1f98f100504838474b88
Yeshey-Gyatso <yesheygyatsoo.yg@gmail.com>
2023-10-07 11:13:53 +0530

    added search

--- packages/ui/components/search.tsx
+++ packages/ui/components/search.tsx
@@ -1,36 +1,35 @@
-import React from 'react'
+"use client";
+import React from 'react';
+import { BiSearch } from 'react-icons/bi';
 
 const Search = () => {
-  return (<div>
-         <div className="mb-3">
-  <div className="relative mb-4 flex w-full flex-wrap items-stretch">
-    <input
-      type="search"
-      className="relative m-0 block w-[1px] min-w-0 flex-auto rounded border border-solid border-neutral-300 bg-transparent bg-clip-padding px-3 py-[0.25rem] text-base font-normal leading-[1.6] text-neutral-700 outline-none transition duration-200 ease-in-out focus:z-[3] focus:border-primary focus:text-neutral-700 focus:shadow-[inset_0_0_0_1px_rgb(59,113,202)] focus:outline-none dark:border-neutral-600 dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:focus:border-primary"
-      placeholder="Search"
-      aria-label="Search"
-      aria-describedby="button-addon2" />
+  const handleSearchClick = () => {
+    
+    alert('Performing search...');
+  };
 
-    {/* search icon */}
-    <span
-      className="input-group-text flex items-center whitespace-nowrap rounded px-3 py-1.5 text-center text-base font-normal text-neutral-700 dark:text-neutral-200"
-      id="basic-addon2">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 20 20"
-        fill="currentColor"
-        className="h-5 w-5">
-        <path
-          fill-rule="evenodd"
-          d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z"
-          clip-rule="evenodd" />
-      </svg>
-    </span>
-  </div>
-</div>
-  </div>
-   
-  )
-}
+  return (
+    <div className="mb-3">
+      <div className="relative mb-4 flex w-full flex-wrap items-stretch">
+        <input
+          type="search"
+          className="relative m-0 block w-full flex-auto rounded border border-solid border-neutral-300 bg-transparent bg-clip-padding px-3 py-[0.25rem] text-base font-normal leading-[1.6] text-neutral-700 outline-none transition duration-200 ease-in-out focus:z-[3] focus:border-primary focus:text-neutral-700 focus:shadow-[inset_0_0_0_1px_rgb(59,113,202)] focus:outline-none dark:border-neutral-600 dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:focus:border-primary"
+          placeholder="Search"
+          aria-label="Search"
+          aria-describedby="button-addon2"
+        />
 
-export default Search
+        {/* search icon */}
+        <button
+          onClick={handleSearchClick}
+          className="absolute right-3 top-1/2 transform -translate-y-1/2 bg-primary text-white p-2 rounded-full cursor-pointer"
+          aria-label="Search Button"
+        >
+          <BiSearch className="h-5 w-5" />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Search;
